35ad56b35c394ccc2a5f343fca18072ee4dd4ab1
Elvia Vallade <ellievallade@gmail.com>
2026-03-29 11:31:21 -0500

    Added Pages

---
+++ Chapter4_Javascript/script.js
@@ -0,0 +1,35 @@
+var menu = `<ul>
+
+  <li><a href="01-Example.html">Example</a></li>
+
+  <li><a href="02-Element.html">Elements</a></li>
+
+  <li><a href="03-Function.html">Function</a></li>
+
+  <li><a href="04-IfElse.html">If/Else</a></li>
+
+  <li><a href="05-Case.html">Case Switch</a></li>
+
+  <li><a href="06-Alert.html">Alert Box</a></li>
+
+  <li><a href="07-Confirm.html">Confirm Box</a></li>
+
+  <li><a href="08-Prompt.html">Prompt</a></li>
+
+  <li><a href="09-Function2.html">Functions 2</a></li>
+
+  <li><a href="10-Iterative.html">Iterative</a></li>
+
+  <li><a href="11-While.html">While Loop</a></li>
+
+  <li><a href="12-Do.html">Do While Loop</a></li>
+
+  <li><a href="13-Break.html">Break & Continue</a></li>
+
+  <li><a href="14-Test.html">Testing Objects</a></li>
+
+  <li><a href="15-JSForms.html">JS Forms</a></li>
+
+  <li><a href="16-JSFormsV.html">Form Validation</a></li>
+
+</ul>`;
